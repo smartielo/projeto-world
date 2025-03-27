b111d67e06b3e4c21a0969a4cf4ff6fe3aea7e2a
Gabriel Martielo <gabrielmartielo3214@gmail.com>
2025-03-26 22:49:44 -0300

    primeiro commit

---
+++ api-world-react/src/main/java/com/gabriel/api_world_react/ApiWorldReactApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.api_world_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWorldReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWorldReactApplication.class, args);
+	}
+
+}
---
+++ api-world-react/src/test/java/com/gabriel/api_world_react/ApiWorldReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.api_world_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWorldReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
